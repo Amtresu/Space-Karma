17a43da0c22731167aa644c35021f362160bafd9
Amtresu <conor.follette@gmail.com>
2022-09-26 21:48:29 +0900

    fixing linter errors

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React, { Component }  from 'react';
 
 test('renders learn react link', () => {
   render(<App />);
