dffd38ce619a8d98da37a22f34e43f3a6368d339
edi-sipka@gmail.com <edisipka@gmail.com>
2022-09-28 17:01:53 +0200

    Add reservation on MyProfile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,10 +1,26 @@
-import React from 'react';
+/* eslint-disable */
 
-function MyProfile() {
+import { useSelector } from 'react-redux';
+import { ListGroup } from 'react-bootstrap';
+
+function Myprofile() {
+  const reservationRocket = useSelector((state) =>
+    state.rocket.filter((rocket) => rocket.reserved)
+  );
   return (
-    <div className="my-profile">
-      <h2>My Profiles</h2>
+    <div className="main">
+      <div className="rockets">
+        <h2>Reserved Rockets</h2>
+        <ListGroup>
+          {reservationRocket.map((v) => (
+            <ListGroup.Item key={v.id}>
+              <h4>{v.name}</h4>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </div>
     </div>
   );
 }
-export default MyProfile;
+
+export default Myprofile;
